267005c327ab0b74d75e755e1e4b01cf8b557f1e
DawnofSouls <28073955+DawnofSouls@users.noreply.github.com>
2025-09-06 15:27:05 -0700

    Added function

---
+++ Week2/test.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int algoA(const int a[], int sz)
+{
+
+    int max, sum = 0;
+
+    for (int i = 0; i < sz; i++)
+    {
+
+        for (int j = 0; j + i < sz; j++)
+        {
+            int start = j;
+            int end = j + i;
+
+            for(start = 0; start<=end; start++)
+            {   
+                cout << a[start] << endl;
+                
+                sum+=a[start];
+                cout << sum << endl;
+            }
+            if (max < sum)
+            {
+                max = sum;
+            }
+            sum = 0;
+
+        }
+    }
+    cout << max << endl;
+    return max;
+};
+
+int main()
+{
+    int n = 7;
+    int Arry[] = {9, 1, -16, 4, 12, -8, 6};
+
+    algoA(Arry, n);
+}
