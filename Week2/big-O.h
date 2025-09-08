4a5811ae88dd7690a36debb68538f80c64181332
DawnofSouls <28073955+DawnofSouls@users.noreply.github.com>
2025-09-07 20:05:21 -0700

    Added prediction and actual runtime info

--- Week2/big-O.h
+++ Week2/big-O.h
@@ -3,30 +3,36 @@ CS311 Kazumi Slott
 
 Your name: Agustin Martinez
 Your program number: 24
-Hours spent: 6 hours
+Hours spent: 8 hours
 Describe any difficulties here: In Algo A, I couldn't find a better way to make one without doing an O(n^3) time complexity. Unsure if that's a good answer.
 
 Experiment:                             (in milliseconds)
                                         AlgoA       AlgoB      AlgoC
 ------------------------------------------------------------------------
 Actual Runtimes with 1000 elements    |           |           |
-(Record the 3 runtimes obtained       |           |           |
+(Record the 3 runtimes obtained       |  202.414  |   1.333   | 0.004
 when you run big-O_file.cpp)          |           |           |
 ------------------------------------------------------------------------
 Expected Runtimes with 2000 elements  |           |           |
-(Predict how long each algo would take|           |           |
+(Predict how long each algo would take|  1619.32  |   5.332   | 0.008
 before you run big-O_file.cpp)        |           |           |
 ------------------------------------------------------------------------
-Actual Runtimes with 2000 elements    |           |           |
+Actual Runtimes with 2000 elements    |  1226.19  |   3.982   | 0.008
 ------------------------------------------------------------------------
-Expected Runtimes with 3000 elements  |           |           |
+Expected Runtimes with 3000 elements  |  5465.178 |  11.997   | 0.011
 ------------------------------------------------------------------------
-Actual Runtimes with 3000 elements    |           |           |
+Actual Runtimes with 3000 elements    |  4049.25  |   8.849   | 0.01
 ------------------------------------------------------------------------
+
+
+
+
 
 <Question> Did the actual runtimes for 2000 and 3000 match what you expected?
 Please explain.
-<Your answer here>
+<I was suprised to see difference between my prediction and the actual runtime. I suppose there's a lot of varibles to this
+including faster machine especially on Algo A and B. Algo C felt easy to predict because it runs at linear complexity so I can multiply 2 or 3 after dividng 
+elements by 1000.>
 
 ************************************************************************/
 
